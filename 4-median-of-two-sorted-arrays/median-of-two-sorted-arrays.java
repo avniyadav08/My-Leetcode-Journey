7a2f762edab0c7af7832d52919867291800d60e3
Avni Yadav <145352116+avniyadav08@users.noreply.github.com>
2026-01-01 23:45:43 +0530

    Time: 2 ms (45.31%) | Memory: 48.7 MB (72.78%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,69 +1,37 @@
-/*class Solution {
+class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m=nums2.length;
+        int m = nums1.length;
+        int n = nums2.length;
+        int result[] = new int[m + n];
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                result[k] = nums1[i];
+                i++;
+                k++;
+            } else {
+                result[k] = nums2[j];
+                k++;
+                j++;
+            }
+        }
+        while (i < m) {
+            result[k] = nums1[i];
+            i++;
+            k++;
 
-        //merge two arrays
-        int[] merged=new int[n+m];
-        int k=0;
-
-        for(int i=0;i<n;i++){
-            merged[k++]=nums1[i];
+        }
+        while (j < n) {
+            result[k] = nums2[j];
+            j++;
+            k++;
+        }
+        if (k % 2 != 0) {
+            k =k/2;
+            return (double) result[k];
+        } else {
+            return ((double) result[k / 2 - 1] + (double) result[k / 2]) / 2.0;
         }
 
-        for(int i=0;i<m;i++){
-            merged[k++]=nums2[i];
-        }
-
-        Arrays.sort(merged);
-        
-       int low=0;int high =merged.length-1;
-       int mid=(high+low)/2;
-       if((n+m)%2==0){
-        //even length
-        return (merged[mid-1]+ merged[mid]) / 2.0;
-       }
-       else{//odd length
-        return merged[mid];
-       }
     }
-}*/
-
-class Solution {  
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {  
-        int n = nums1.length;  
-        int m = nums2.length;  
-
-        // Merge two arrays  
-        int[] merged = new int[n + m];  
-        int k = 0;  
-        int i = 0;  
-        int j = 0;  
-
-        while (i < n && j < m) {  
-            if (nums1[i] < nums2[j]) {  
-                merged[k++] = nums1[i++];  
-            } else {  
-                merged[k++] = nums2[j++];  
-            }  
-        }  
-
-        while (i < n) {  
-            merged[k++] = nums1[i++];  
-        }  
-
-        while (j < m) {  
-            merged[k++] = nums2[j++];  
-        }  
-
-        int mid = (n + m) / 2;  
-
-        if ((n + m) % 2 == 0) {  
-            // Even length  
-            return (merged[mid - 1] + merged[mid]) / 2.0;  
-        } else {  
-            // Odd length  
-            return merged[mid];  
-        }  
-    }  
 }
