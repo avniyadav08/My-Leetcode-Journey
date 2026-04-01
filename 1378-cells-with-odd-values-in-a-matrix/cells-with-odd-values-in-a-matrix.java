ed5f3fb9babccf69572c7d5e220b53576e5c6282
Avni Yadav <145352116+avniyadav08@users.noreply.github.com>
2026-04-01 19:37:59 +0530

    Time: 2 ms (43.63%) | Memory: 43.6 MB (37.01%) - LeetSync

--- 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.java
@@ -1,22 +1,34 @@
 class Solution {
     public int oddCells(int n, int m, int[][] indices) {
-        int count = 0;
-        int row[] = new int [n];
-        int col[] = new int [m];
-        for(int x[] : indices)
-        {
-            row[x[0]]++;
-            col[x[1]]++;
-        }    
-
-      
+        // Step 1: Create the matrix
+        int[][] matrix = new int[n][m];
         
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-            { 
-                if((row[i]+col[j])%2!=0)
+        // Step 2: Apply each operation directly
+        for (int[] op : indices) {
+            int row = op[0];
+            int col = op[1];
+            
+            // Increment all cells in the given row
+            for (int j = 0; j < m; j++) {
+                matrix[row][j]++;
+            }
+            
+            // Increment all cells in the given column
+            for (int i = 0; i < n; i++) {
+                matrix[i][col]++;
+            }
+        }
+        
+        // Step 3: Count odd cells
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] % 2 != 0) {
                     count++;
-            }        
+                }
+            }
+        }
+        
         return count;
     }
 }
