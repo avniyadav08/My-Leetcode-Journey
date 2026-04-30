9e67ec891c39968624b879dec48296ee8e7ed792
Avni Yadav <145352116+avniyadav08@users.noreply.github.com>
2026-04-30 19:46:55 +0530

    Time: 4 ms (8.50%) | Memory: 43.8 MB (5.82%) - LeetSync

---
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int[] copy = nums.clone();
+        int n = copy.length;
+
+        Arrays.sort(copy);
+
+        int secondHighest = copy[n - 2];
+        int highest = copy[n - 1];
+
+
+        if (highest >= 2 * secondHighest) {
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == highest) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
